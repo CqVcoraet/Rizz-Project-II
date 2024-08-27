d6e411c88c874aa2787a191bce8f366dc9cf14f9
CqVcoraet <149435569+CqVcoraet@users.noreply.github.com>
2024-08-27 11:12:03 -0600

    First commit.

---
+++ Rizz.java
@@ -0,0 +1,18 @@
+// Imported Packages
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Rizz {
+    // Attributes
+    private String name;
+    private int age;
+    private double rizzStrength;
+    private boolean rizzByDefault;
+    private double version;
+    private String description;
+
+}
